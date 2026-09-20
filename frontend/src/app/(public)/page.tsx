da2f7fa3ae4c3e68aa6c42e2754527207f75651c
Felipe Tajima <felipebtajima05@gmail.com>
2026-09-20 14:36:08 -0300

    feat(public-home): add Plans, HowItWorks, Contact and Differentials

--- frontend/src/app/(public)/page.tsx
+++ frontend/src/app/(public)/page.tsx
@@ -1,7 +1,11 @@
 import { HeroSection } from "@/features/public-home/components/HeroSection";
 import { AudienceSection } from "@/features/public-home/components/AudienceSection";
 import { ProductSection } from "@/features/public-home/components/ProductSection";
-import { ServicesSection } from "@/features/public-home/components/ServicesSection";  
+import { ServicesSection } from "@/features/public-home/components/ServicesSection";
+import { HowItWorksSection } from "@/features/public-home/components/HowItWorksSection";
+import { DifferentialsSection } from "@/features/public-home/components/DifferentialsSection";
+import { ContactSection } from "@/features/public-home/components/ContactSection";
+import { PlansSection } from "@/features/public-home/components/PlansSection";
 
 export default function Home() {
   return (
@@ -10,6 +14,10 @@ export default function Home() {
       <AudienceSection />
       <ProductSection />
       <ServicesSection />
+      <HowItWorksSection />
+      <DifferentialsSection />
+      <PlansSection />
+      <ContactSection />
     </>
   );
 }
